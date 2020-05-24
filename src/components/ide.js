d41c7389fd2d84f4fead89715a9b528993110f46
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-05-25 02:52:02 +0530

    Added save feature

--- src/components/ide.js
+++ src/components/ide.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import AceEditor from "react-ace";
 import "../App.css";
 
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
 import { withRouter } from "react-router-dom";
+import { GlobalHotKeys } from "react-hotkeys";
+
+const keyMap = {
+  SAVE: "ctrl+shift+s",
+};
 
 const IDE = withRouter((props) => {
-  const file = props.location.state.file;
+  var [file, setFile] = useState(props.location.state.file);
+  
+  const saveFile = () => {
+    const element = document.createElement("a");
+    const newFile = new Blob([file.contents], {type: 'text/plain'});
+    element.href = URL.createObjectURL(newFile);
+    element.download = file.name;
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  };
+
+  const handlers = {
+    SAVE: saveFile,
+  };
+
   console.log(file);
   function onChange(newValue) {
-    console.log("change", newValue);
+    var temp = file;
+    temp.contents = newValue;
+    setFile(temp);
   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{file.name}</p>
-        <AceEditor
-          mode="java"
-          theme="github"
-          value={file.contents}
-          onChange={onChange}
-          name="UNIQUE_ID_OF_DIV"
-          editorProps={{ $blockScrolling: true }}
-        />
-      </header>
-    </div>
+    <GlobalHotKeys keyMap={keyMap} handlers={handlers}>
+      <div className="App">
+        <header className="App-header">
+          <p>{file.name}</p>
+          <p>Ctrl + Shift + S to save</p>
+          <AceEditor
+            mode="java"
+            theme="github"
+            value={file.contents}
+            onChange={onChange}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+          />
+        </header>
+      </div>
+    </GlobalHotKeys>
   );
 });
 export default IDE;
